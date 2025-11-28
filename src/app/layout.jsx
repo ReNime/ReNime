bc469f5a227d819bbaf0fc87897ad643d7fb1668
nekotopup <akungenshinku39@gmail.com>
2025-11-29 05:45:21 +0700

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,8 +18,8 @@ export const metadata = {
   icons: {
     icon: [
       { url: '/images/favicon_io/favicon.ico' },
-      { url: '/images/favicon_io/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/images/favicon_io/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/images/favicon_io/favicon-32x32.png', sizes: '16x16', type: 'image/png' },
+      { url: '/images/favicon_io/favicon-64x64.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
       { url: '/images/favicon_io/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
