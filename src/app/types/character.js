6b6264e7e17ce92b5b93f1a8a50f92dfe987efda
nekotopup <akungenshinku39@gmail.com>
2025-11-29 07:01:58 +0700

    Create character.js

---
+++ src/app/types/character.js
@@ -0,0 +1,18 @@
+// No TypeScript types needed — just export empty objects or JSDoc for hints
+
+/**
+ * @typedef {Object} Character
+ * @property {number} id
+ * @property {{ full: string }} name
+ * @property {{ large: string }} image
+ */
+
+/**
+ * @typedef {Object} VoiceActor
+ * @property {number} id
+ * @property {{ full: string }} name
+ * @property {{ large: string }} image
+ * @property {string} language
+ */
+
+module.exports = {};
