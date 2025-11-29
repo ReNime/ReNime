ca2179cac0dbcb2402f1f30ec071abf2df62b12c
nekotopup <akungenshinku39@gmail.com>
2025-11-29 07:12:24 +0700

    Update page.jsx

--- src/app/renai/page.jsx
+++ src/app/renai/page.jsx
@@ -9,7 +9,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { signIn, useSession } from "next-auth/react";
-import { searchAnimeByFile } from "@/lib/traceMoe";
+import { searchAnimeByFile } from "@/libs/traceMoe";
 import Head from "next/head";
 
 export default function AichixiaPage() {
