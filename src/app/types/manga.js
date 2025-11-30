264571f1eaae3fe2ba9392b16c9ff184b44f5968
nekotopup <akungenshinku39@gmail.com>
2025-11-30 15:39:01 +0700

    Create manga.js

---
+++ src/app/types/manga.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} Manga
+ * @property {string} id 
+ * @property {string} title
+ * @property {string} coverFileName
+ * @property {string} [description]
+ * @property {string} [author]
+ * @property {number} [rating]
+ * @property {string[]} [genres]
+ * @property {number} [year]
+ * @property {string} [status]
+ * @property {string} [lastChapter]
+*/
