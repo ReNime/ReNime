56c8ad96feb18c6ce77ea631ee5e0888a10924f1
nekotopup <akungenshinku39@gmail.com>
2026-01-09 22:41:50 +0700

    Update MangaGrid.jsx

--- src/app/components/MangaGrid.jsx
+++ src/app/components/MangaGrid.jsx
@@ -1,11 +1,5 @@
-import { getLocalizedTitle, getCoverImage } from '@/app/libs/mangadex'
+import { getLocalizedTitle } from '@/app/libs/mangadex'
 import MangaCard from './MangaCard'
-
-/**
- * @typedef {Object} MangaGridProps
- * @property {Array<any>} mangaList
- */
-
 
 export default function MangaGrid({ mangaList }) {
   if (!mangaList || mangaList.length === 0) {
@@ -15,9 +9,13 @@ export default function MangaGrid({ mangaList }) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
       {mangaList.map((manga) => {
-        const title = getLocalizedTitle(manga.attributes.title)
-        const cover = manga.relationships.find((rel) => rel.type === 'cover_art')
-        const coverFileName = cover?.attributes?.fileName || ''
+        const title = getLocalizedTitle(manga.attributes?.title || {})
+
+        const coverRel = manga.relationships?.find(
+          (rel) => rel.type === 'cover_art'
+        )
+
+        const coverFileName = coverRel?.attributes?.fileName
 
         if (!coverFileName) return null
 
