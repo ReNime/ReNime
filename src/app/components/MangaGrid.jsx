69090a77e0b180074737a4a477805b9f7348719c
nekotopup <akungenshinku39@gmail.com>
2026-01-10 09:12:52 +0700

    Update MangaGrid.jsx

--- src/app/components/MangaGrid.jsx
+++ src/app/components/MangaGrid.jsx
@@ -13,6 +13,7 @@ export default function MangaGrid({ mangaList }) {
           slug={manga.slug}
           title={manga.title}
           thumbnail={manga.thumbnail}
+          chaptersCount={manga.chaptersCount}
         />
       ))}
     </div>
