e872e1c3cdb16a17cc20c2975ed6bf82fa7b4224
nekotopup <akungenshinku39@gmail.com>
2026-01-12 13:02:28 +0700

    Update LightNovelCard.jsx

--- src/app/components/LightNovelCard.jsx
+++ src/app/components/LightNovelCard.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function LightNovelCard({ slug, title }) {
+export default function LightNovelCard({ slug, title, cover}) {
   const fallbackCover = '/default-cover.jpg'
 
   return (
