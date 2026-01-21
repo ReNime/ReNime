15350f27455a6d9dd9182a081418d5341f006ba7
nekotopup <akungenshinku39@gmail.com>
2026-01-21 13:45:00 +0700

    Update route.js

--- src/app/api/manga/favorites/route.js
+++ src/app/api/manga/favorites/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
-import { getAuthSession } from '@/src/libs/auth-libs'
-import { prisma } from '@/src/libs/prisma'
+import { getAuthSession } from '@/libs/auth-libs'
+import { prisma } from '@/libs/prisma'
 
 /**
  * GET /api/manga/favorites
